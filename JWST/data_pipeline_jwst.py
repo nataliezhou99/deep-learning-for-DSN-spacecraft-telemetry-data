b9e2de25a57d042d574723c306fffe529f34f7e4
nataliezhou99 <nzhou@caltech.edu>
2025-09-24 16:50:03 -0700

    Update data_pipeline_jwst.py

--- JWST/data_pipeline_jwst.py
+++ JWST/data_pipeline_jwst.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import MinMaxScaler
 from tqdm import tqdm 
 
 # --- 1. CONFIGURATION --- 
-PROJECT_DIR = Path("/home/nzhou/updated_dsn_project/JWSTData") 
+PROJECT_DIR = Path("/home/nzhou/JWSTDataset") 
 CHUNK_FILE_PATTERN = "chunk_*_mon_JWST.pkl.gz" 
 DRS_FILE = "all_dr_data.csv" 
 OUTPUT_DIR = PROJECT_DIR / "processed_diffusion_style" 
